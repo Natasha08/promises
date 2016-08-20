27211f50f1eb9646febb21f76a6448497c396e8e
natasha <natasha08@me.com>
2016-08-20 08:28:07 -0600

    adds a non evergreen test.

--- promiseTest.js
+++ promiseTest.js
@@ -12,19 +12,28 @@ describe('es6 promises', function() {
     let  result = { id: "ExternalId" };
 
     sinon.stub(repository, 'create').returns(Promise.resolve(result));
-  });
 
-  it("checks if myPromise is defined", function () {
+    it("checks if myPromise is defined", function () {
 
-      expect(repository.create()).not.be.undefined;
-  });
+        expect(repository.create()).not.be.undefined;
+    });
 
-  it("gets the resolved data from the promise", function () {
-      let myPromise = repository.create();
+    it("gets the resolved data from the promise", function () {
+        let myPromise = repository.create();
 
-      return myPromise.then(function(data){
-        console.log("data", data);
-        expect(data).to.eql( { id: "ExternalId" } );
-      });
+        return myPromise.then(function(data){
+          console.log("data", data);
+          expect(data).to.eql( { id: "ExternalId" } );
+        });  
+    });
+
+    it("does not become evergreen", function () {
+        let myPromise = repository.create();
+
+        return myPromise.then(function(data){
+          console.log("data", data);
+          expect(data).not.to.eql( { id: "ExternalId" } );
+        });
+    });
   });
 });
